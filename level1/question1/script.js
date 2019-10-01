4fe0f8eb1a6294e698efa54b9b2ac49965c77ba5
kriss <krisyv3@hotmail.com>
2019-10-01 11:24:33 +0200

    Regex that checks the validity of the number and changes the border color based on true or false.

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,14 @@
 //Use RegEx to validate form
+
+function myfunction() {
+    let numPatt = /0\d{5}7/;
+    let phoneValue = document.getElementById(phoneNumber).Value;
+  
+
+    if (numPatt.test(phoneValue) === true) {
+        document.getElementById(phoneNumber).style.bordercolor = "green";
+    }
+    else {
+        document.getElementById(phoneNumber).style.bordercolor = "red";
+    };
+};
