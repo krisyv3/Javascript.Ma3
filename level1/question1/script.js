b1319daec898666386e5f3664c8887221de5121a
kriss <krisyv3@hotmail.com>
2019-10-02 09:23:51 +0200

    All regex Functions are done and Working.

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,15 +1,62 @@
 //Use RegEx to validate form
 
-function CheckNumber() {
+function checkEmail() {
+    var emailPatt = /(.+)@(.+){2,}\.(.+){2,}/
+    var emailValue = document.getElementById("email");
+    var emailFinalValue = emailPatt.test(emailValue.value);
+
+    if (emailFinalValue === true) {
+        console.log("Email is OK!");
+    }
+    else {
+        console.log("email is missing!");
+    };
+};
+
+function checkLastName() {
+    var lastNamePatt = /\S+/;
+    var lastNameValue = document.getElementById("lName");
+    var lastNameFinalValue = lastNamePatt.test(lastNameValue.value);
+
+    if (lastNameFinalValue === true) {
+        console.log("Last name is OK");
+    }
+    else {
+        console.log("last name is missing!");
+    };
+};
+
+function checkName() {
+    var namePatt = /\S+/;
+    var nameValue = document.getElementById("fName");
+    var nameFinalValue = namePatt.test(nameValue.value);
+
+    if (nameFinalValue === true) {
+        console.log("First name is OK");
+    }
+    else {
+        console.log("First name is missing!")
+    };
+};
+
+
+function checkNumber() {
     var numPatt = /\d{7}/;
     var phoneValue = document.getElementById("phoneNumber");
     var finalValue = numPatt.test(phoneValue.value);
    
     
     if (finalValue === true) {
-        console.log("it lives");
+        console.log("PhoneNumber is OK!");
     }
     else {
-        console.log("something's wrong");
+        console.log("PhoneNumber is missing!");
     };
 };
+
+function multipleFunctions() {
+    checkNumber();
+    checkName();
+    checkLastName();
+    checkEmail();
+}
