b39c0eda65c25b0b27f844e35d56b66c80cf965b
kriss <krisyv3@hotmail.com>
2019-10-02 15:40:20 +0200

    Done with JSON loop
    I moved the content from author.json to the JS file, i tried to use "fetch api" but i was encountered by some issues since the files were local

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,21 +1,34 @@
 //Console.log out elements in JSON file
 
 
-function parseJSON() {
-var obj = fetch('author.json').then(function(response) {
-    return response.json();
-  }).then(function(parsedJson) {
-    console.log(parsedJson);
-  });
- 
-var myArray = obj;
-var i = 0;
+function logJson() {
 
-for (i < myArray.lenght; i++) {
-  
-}
+  var i;
+  var myArray =
+  {
+    "video": [{
+      "id": 12312412312,
+      "name": "Ecuaciones Diferenciales",
+      "url": "/video/math/edo/12312412312",
+      "author": {
+        "data": [{
+          "name_author": "Alejandro Morales",
+          "url": "/author/alejandro-morales",
+          "type": "master"
+        }]
+      }
+    }]
+  };
 
+  var entries = Object.entries(myArray.video[0])
+  var secondEndtries = Object.entries(myArray.video[0].author.data[0]);
+
+  for( i = 0; i < entries.length; i++) {
+    console.dir(entries[i]);
+  }
+
+  for( i = 0; i < secondEndtries.length; i++) {
+    console.dir(secondEndtries[i]);
+  }
 
 };
-
-
